1ff266943933c8e72719bbd90ef418e273b5c451
Alex Iancu <alexandru.iancu@gmail.com>
2025-10-02 21:26:41 +0300

    refactor: make 'PORT' configuration reusable

--- src/todoui-cpp/driver.cpp
+++ src/todoui-cpp/driver.cpp
@@ -1,21 +1,38 @@
 #include <cstdlib>
 #include <unordered_map>
 #include <string>
+#include <variant>
 
 #include "crow.h"
 
 int main(int argc, char *argv[]) {
   // set defaults
-  std::unordered_map<std::string, std::string> app_config={{"BACKEND_URL", "http://localhost:8080/todos/"}};
+  using ConfigValT = std::variant<std::string, uint32_t>;
+  std::unordered_map<std::string, ConfigValT> app_config={
+          {"PORT", (uint32_t)8080},
+          {"URL", "http://localhost:{}/todos/"}
+  };
+
+  // update URL
+  app_config["URL"] = std::vformat(std::get<std::string>(app_config["URL"]),
+		  std::make_format_args(
+			  std::get<uint32_t>(app_config["PORT"])
+			  ) 
+		  );
 
   // update valued with environment
   {
-    std::string sEnvironmentBackend = std::getenv("BACKEND_URL");
-    if (!sEnvironmentBackend.empty())
-      app_config["BACKEND_URL"] = sEnvironmentBackend;
+    const char *pszEnvBackendURL = std::getenv("URL");
+    if (pszEnvBackendURL != nullptr)
+      app_config["URL"] = pszEnvBackendURL;
   }
 
   crow::SimpleApp app;
-  app.port(8080).multithreaded().run();
+  CROW_ROUTE(app, "/html")([](){
+      // Simple get request and responce as html
+      auto page = crow::mustache::load("index.html");
+      return page.render();
+  });
+  app.port(std::get<uint32_t>(app_config["PORT"])).multithreaded().run();
   return 0;
 }
