26600ffaab692784ea0a376a2377c928232ab65b
Alex Iancu <alexandru.iancu@gmail.com>
2025-10-04 19:22:07 +0300

    feat: add logging feat: implement /add /delete REST endpoints logic(partial)

--- src/todoui-cpp/driver.cpp
+++ src/todoui-cpp/driver.cpp
@@ -4,43 +4,131 @@
 #include <variant>
 
 #include "crow.h"
+//TODO integrate CPR
+//#include "cpr/cpr.h"
+
+//template<typename T>
+//const T& unmove(T&& t) {
+//  return t;
+//}
+
+class LogHandler : public crow::ILogHandler
+{
+public:
+    void log(const std::string& message, crow::LogLevel /*level*/) override
+    {
+        std::cerr << "LogHandler -> " << message;
+    }
+};
 
 int main(int argc, char *argv[]) {
   // set defaults
   using ConfigValT = std::variant<std::string, uint32_t>;
   std::unordered_map<std::string, ConfigValT> app_config={
-          {"PORT", (uint32_t)8080},
-          {"URL", "http://localhost:{}/todos/"}
+          {"FRONTEND_PORT", (uint32_t)5000},
+//          {"BACKEND_PORT", (uint32_t)8080},
+//          {"BACKEND_URL", "http://localhost{}/todos/"}
+          {"BACKEND_URL", "http://localhost:8080/todos/"}
   };
 
-  // update URL
-  app_config["URL"] = std::vformat(std::get<std::string>(app_config["URL"]),
-		  std::make_format_args(
-			  std::get<uint32_t>(app_config["PORT"])
-			  ) 
-		  );
+  crow::logger::setLogLevel(crow::LogLevel::Debug);
+  //crow::logger::setLogLevel(crow::LogLevel::Info);
+  //crow::logger::setLogLevel(crow::LogLevel::Warning);
 
-  // update valued with environment
+  //// update BACKEND_URL
+  //if (app_config.contains("BACKEND_PORT")) {
+  //  CROW_LOG_DEBUG << std::format("Debug | config BACKEND_URL: {}", std::get<uint32_t>(app_config["BACKEND_PORT"]));
+//
+  //  app_config["BACKEND_URL"] = std::vformat(std::get<std::string>(app_config["BACKEND_URL"]),
+  //      std::make_format_args(
+  //        unmove(std::string(":") += std::get<uint32_t>(app_config["BACKEND_PORT"]))
+  //        )
+  //      );
+  //}
+
+  // update with environment
   {
-    const char *pszEnvBackendURL = std::getenv("URL");
+    const char *pszEnvFrontendPort = std::getenv("BACKEND_PORT");
+    if (pszEnvFrontendPort != nullptr)
+      app_config["FRONTEND_PORT"] = pszEnvFrontendPort;
+
+    const char *pszEnvBackendURL = std::getenv("BACKEND_URL");
     if (pszEnvBackendURL != nullptr)
-      app_config["URL"] = pszEnvBackendURL;
+      app_config["BACKEND_URL"] = pszEnvBackendURL;
   }
 
   crow::SimpleApp app;
-  CROW_ROUTE(app, "/")([](){
+
+  CROW_ROUTE(app, "/")([&app_config](){
       auto page = crow::mustache::load("index.html");
+      CROW_LOG_INFO << std::format("GET {}/todos/", 
+        std::get<std::string>(app_config["BACKEND_URL"])
+      );
       return page.render();
   });
-  CROW_ROUTE(app, "/add").methods("POST"_method)(
-    [](const crow::request& req){
-      return crow::response{""};
-  });
-  CROW_ROUTE(app, "/delete").methods("POST"_method)(
-    [](const crow::request& req){
-      return crow::response{""};
+
+  CROW_ROUTE(app, "/add")
+  .methods("POST"_method)([&app_config](const crow::request& req){
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //Debug
+    crow::multipart::message multi_req(req);
+    std::ostringstream oss;
+    for (auto &[key, part] : multi_req.part_map) {
+      oss << key << ": " << part.body << "\n";
+    }
+    CROW_LOG_DEBUG << oss.str();
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    auto new_todo = multi_req.get_part_by_name("todo");
+    CROW_LOG_INFO << std::format("POST  {}/todos/{}",
+        std::get<std::string>(app_config["BACKEND_URL"]), new_todo.body
+    );
+
+    //TODO integrate CPR
+    //auto cpr_resp = cpr::Post(
+    //    cpr::Url{std::get<std::string>(app_config["BACKEND_URL"])},
+    //    cpr::Body{new_todo.body},
+    //    cpr::Header{{"Content-Type", "application/json"}}
+    //);
+    //CROW_LOG_DEBUG << "Add| CPR status code: " << cpr_resp.status_code << " CPR response: " << response.text;
+
+    crow::response resp;
+    resp.redirect("/");
+    return resp;
   });
 
-  app.port(std::get<uint32_t>(app_config["PORT"])).multithreaded().run();
+  CROW_ROUTE(app, "/delete")
+  .methods("POST"_method)([&app_config](const crow::request& req){
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //Debug
+    crow::multipart::message multi_req(req);
+    std::ostringstream oss;
+    for (auto &[key, part] : multi_req.part_map) {
+      oss << key << ": " << part.body << "\n";
+    }
+    CROW_LOG_DEBUG << oss.str();
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    auto delete_todo = multi_req.get_part_by_name("todo");
+    CROW_LOG_INFO << std::format("POST  {}/todos/{}",
+      std::get<std::string>(app_config["BACKEND_URL"]), delete_todo.body
+    );
+    //TODO integrate CPR
+    //auto cpr_resp = cpr::Post(
+    //    cpr::Url{std::get<std::string>(app_config["BACKEND_URL"])},
+    //    cpr::Body{delete_todo.body},
+    //    cpr::Header{{"Content-Type", "application/json"}}
+    //);
+    //CROW_LOG_DEBUG << "Delete| CPR status code: " << cpr_resp.status_code << " CPR response: " << response.text;
+
+    crow::response resp;
+    resp.redirect("/");
+    return resp;
+  });
+
+  app.port(
+    std::get<uint32_t>(app_config["FRONTEND_PORT"])
+  ).multithreaded().run();
+
   return 0;
 }
